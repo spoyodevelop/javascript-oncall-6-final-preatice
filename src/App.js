9d11215bf6d3d845b5ea45925fac7ea04323440a
spoyodevelop <kjy9505@gmail.com>
2024-12-13 19:26:37 +0900

    feat: 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
+import { resolvePlugin } from '@babel/core';
 import InputView from './View/InputView.js';
 import Calendar from './Calendar.js';
+import OutputView from './View/OutputView.js';
 
 class App {
   async run() {
     // const input = await InputView.getMouthAndDay();
-    // const { monthAndDay, normalCrews, holidayCrews } =
-    //   await InputView.getInputs();
+    const { monthAndDay, normalCrews, holidayCrews } =
+      await InputView.getInputs();
 
-    const normalCrews =
-      '준팍,도밥,고니,수아,루루,글로,솔로스타,우코,슬링키,참새,도리'.split(',');
+    // const normalCrews =
+    //   '준팍,도밥,고니,수아,루루,글로,솔로스타,우코,슬링키,참새,도리'.split(',');
 
-    const holidayCrews =
-      '수아,루루,글로,솔로스타,우코,슬링키,참새,도리,준팍,도밥,고니'.split(',');
+    // const holidayCrews =
+    //   '수아,루루,글로,솔로스타,우코,슬링키,참새,도리,준팍,도밥,고니'.split(',');
     function rotateCrew(crews) {
       const pickedOne = crews.shift();
       crews.push(pickedOne);
-      return { pickedOne, crews };
+      return pickedOne;
+    }
+    function checkAvaliablity(onCall, pickedOne) {
+      return onCall[onCall.length - 1] === pickedOne;
     }
 
-    const cal = new Calendar(5, '월').calendar;
+    const { month, day } = monthAndDay;
+
+    const cal = new Calendar(month, day).calendar;
+
     const onCall = [];
-    cal.forEach(({ month, day, dayOfWeek, isHoliday }) => {
-      // if (onCall. || )
+    let reserved = '';
+    cal.forEach((date) => {
+      if (reserved !== '') {
+        onCall.push(reserved);
+        reserved = '';
+      } else if (
+        date.dayOfWeek !== '토' &&
+        date.dayOfWeek !== '일' &&
+        !date.isHoliday
+      ) {
+        const pickedOne = rotateCrew(normalCrews);
+        if (checkAvaliablity(onCall, pickedOne)) {
+          reserved = pickedOne;
+          const pickedAgainOne = rotateCrew(normalCrews);
+          onCall.push(pickedAgainOne);
+        } else onCall.push(pickedOne);
+      } else {
+        const pickedOne = rotateCrew(holidayCrews);
+
+        if (checkAvaliablity(onCall, pickedOne)) {
+          reserved = pickedOne;
+          const pickedAgainOne = rotateCrew(holidayCrews);
+          onCall.push(pickedAgainOne);
+        } else onCall.push(pickedOne);
+      }
     });
+    OutputView.printOnCall(cal, onCall);
   }
 }
 
--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -4,52 +4,12 @@ const OutputView = {
   printMessage(message) {
     Console.print(message);
   },
-  displayWelcomeMessage() {
-    this.printMessage('숫자 야구 게임을 시작합니다.');
-  },
-  displayProducts(products) {
-    this.printMessage('안녕하세요. W편의점입니다.');
-    this.printMessage('현재 보유하고 있는 상품입니다.\n');
-
-    products.forEach((product) => this.printMessage(product.toString()));
-  },
-  displayEachOneBill(eachBill) {
-    this.printMessage('==============W 편의점================');
-    this.printMessage('상품명     수량     금액');
-    eachBill.forEach(({ name, quantity, price }) => {
-      this.printMessage(
-        `${name}     ${quantity}      ${price.toLocaleString()}`,
-      );
+  printOnCall(cal, onCall) {
+    let i = 0;
+    cal.forEach((date) => {
+      Console.print(`${date.toString()} ${onCall[i]}`);
+      i++;
     });
-  },
-
-  displayFreebie(freebieBill) {
-    this.printMessage('=============증 정===============');
-    freebieBill.forEach(({ freebie, name }) => {
-      this.printMessage(`${name}     ${freebie}`);
-    });
-  },
-
-  displayTotal({
-    totalBeforeDiscount,
-    totalQuantity,
-    promoDiscount,
-    membershipDiscount,
-    total,
-  }) {
-    this.printMessage('====================================');
-    this.printMessage(
-      `총구매액            ${totalQuantity}     ${totalBeforeDiscount.toLocaleString()}`,
-    );
-    this.printMessage(
-      `행사할인                       -${promoDiscount.toLocaleString()}`,
-    );
-    this.printMessage(
-      `맴버쉽할인                     -${membershipDiscount.toLocaleString()}`,
-    );
-    this.printMessage(
-      `내실돈                            ${total.toLocaleString()}`,
-    );
   },
 };
 
